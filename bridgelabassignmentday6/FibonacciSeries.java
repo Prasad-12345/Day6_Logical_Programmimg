1cc80f981a1316509318560ec3185c0be3297450
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 12:58:14 +0530

    Added: program to print fibonacci series till nth term

---
+++ bridgelabassignmentday6/FibonacciSeries.java
@@ -0,0 +1,15 @@
+package com.bridgelabassignmentday6;
+/*
+ * Authot: Prasad
+ * program to print fibonacci series till nth term
+ */
+import utility.Util;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		
+		Util.fibonacciSeries();
+	}
+
+}
