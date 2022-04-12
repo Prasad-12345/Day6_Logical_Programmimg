e8b88e8d533719807e0183430254bfffb3ce3131
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-12 13:05:32 +0530

    Added: Program to check entered number is perfect number or not

---
+++ bridgelabassignmentday6/PerfectNumber.java
@@ -0,0 +1,15 @@
+package com.bridgelabassignmentday6;
+/*
+ * Author: Prasad
+ * Program to check entered number is perfect number or not
+ */
+import utility.Util;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		
+		Util.perfectNumber();
+	}
+
+}
